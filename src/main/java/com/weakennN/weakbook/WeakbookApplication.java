fb1e53a58a643aa37ee1ea19f6cde92c2ed93794
weakennN <lubomedi03@gmail.com>
2022-01-13 19:39:06 +0200

    Initial commit

---
+++ src/main/java/com/weakennN/weakbook/WeakbookApplication.java
@@ -0,0 +1,13 @@
+package com.weakennN.weakbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeakbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeakbookApplication.class, args);
+	}
+
+}
