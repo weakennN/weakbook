161eb7f707526ab8e3e84894f334091bd44b4cca
weakennN <lubomedi03@gmail.com>
2022-04-10 14:14:05 +0300

    updated profile page design

--- src/main/resources/static/js/post/PostManager.js
+++ src/main/resources/static/js/post/PostManager.js
@@ -5,6 +5,7 @@ class PostManager {
 
     static init(url) {
         this.#url = url;
+        PostManager.getPosts();
         $(window).scroll(function () {
             if ($(window).scrollTop() + $(window).height() > $(document).height() - 400) {
                 PostManager.getPosts(PostManager.#url);
